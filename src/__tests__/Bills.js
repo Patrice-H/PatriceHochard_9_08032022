a5d978e16f591f5a174aa8513049078af906110b
Patrice-H <https://github.com/>
2022-03-27 13:46:30 +0200

    Code reformat

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,56 +2,56 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor, fireEvent} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import Bills from "../containers/Bills.js"
-import { bills } from "../fixtures/bills.js"
+import {screen, waitFor, fireEvent} from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import Bills from "../containers/Bills.js";
+import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH, ROUTES} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 import router from "../app/Router.js";
+
 jest.mock("../app/store", () => mockStore);
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
+      }));
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId('icon-window'));
+      const windowIcon = screen.getByTestId('icon-window');
       expect(windowIcon.classList.contains('active-icon')).toBe(true);
+    });
 
-    })
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML);
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
 
   describe("When I am on Bills Page and I click on the New Bill button", () => {
     test("Then, it should render NewBill page", () => {
-
       // Init localStorage
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
-      }))
+      }));
 
       // Init onNavigate and store
       const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
+        document.body.innerHTML = ROUTES({ pathname });
       }
-      const store = null
+      const store = null;
 
       // Build user interface
       document.body.innerHTML = BillsUI({ data: bills });
@@ -78,8 +78,8 @@ describe("Given I am connected as an employee", () => {
       expect(handleClickNewBill).toBeCalled();
       // screen should show "Envoyer une note de frais"
       expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy();
-    })
-  })
+    });
+  });
 
   describe("When I am on Bills Page and I click on the icon eye", () => {
     test("Then the modal should open up", () => {
@@ -148,7 +148,7 @@ describe("Given I am connected as an employee", () => {
       const billRefused  = screen.getByText('test1');
       const billAccepted = screen.getByText('test3');
 
-      expect(spy).toHaveBeenCalled()
+      expect(spy).toHaveBeenCalled();
       expect(bills.length).toBe(4);
       expect(billPending).toBeTruthy();
       expect(billRefused).toBeTruthy();
@@ -180,7 +180,7 @@ describe("Given I am connected as an employee", () => {
       mockStore.bills.mockImplementationOnce(() => {
         return {
           list : () =>  {
-            return Promise.reject(new Error("Erreur 404"))
+            return Promise.reject(new Error("Erreur 404"));
           }
         }
       });
@@ -195,7 +195,7 @@ describe("Given I am connected as an employee", () => {
       mockStore.bills.mockImplementationOnce(() => {
         return {
           list : () =>  {
-            return Promise.reject(new Error("Erreur 500"))
+            return Promise.reject(new Error("Erreur 500"));
           }
         }
       });
